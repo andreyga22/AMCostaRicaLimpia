5ec81465aedb83a1e1a9fe9e8fb4cef2480aa055
Andrey Gonzalez Artaviaasdasdaasdf <andreyga22@outlook.com>
2019-04-24 15:36:48 -0600

    Cambiado la interfaz grafica

--- ProyectoAMCRL/ProyectoAMCRL/RegistroSociosUI.aspx.designer.cs
+++ ProyectoAMCRL/ProyectoAMCRL/RegistroSociosUI.aspx.designer.cs
@@ -40,15 +40,6 @@ namespace ProyectoAMCRL {
         protected global::System.Web.UI.WebControls.Button ok_btn;
         
         /// <summary>
-        /// rolRadios control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButtonList rolRadios;
-        
-        /// <summary>
         /// idTB control.
         /// </summary>
         /// <remarks>
@@ -146,6 +137,15 @@ namespace ProyectoAMCRL {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox correoTB;
+        
+        /// <summary>
+        /// rolRadios control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList rolRadios;
         
         /// <summary>
         /// btnRegistrar control.
