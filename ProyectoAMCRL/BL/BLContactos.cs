02a30a82483248b9d8de8e554353b97910a3e6e8
Steven Aguilar <timosnes@gmail.com>
2019-06-10 13:01:48 -0600

    insertar socios

---
+++ ProyectoAMCRL/BL/BLContactos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class BLContactos
+    {
+        public int telefono_hab;
+        public int telefono_pers;
+        public string email;
+
+        public BLContactos(int telefono_hab, int telefono_pers, string email) {
+            this.telefono_hab = telefono_hab;
+            this.telefono_pers = telefono_pers;
+            this.email = email;
+        }
+    }
+}
