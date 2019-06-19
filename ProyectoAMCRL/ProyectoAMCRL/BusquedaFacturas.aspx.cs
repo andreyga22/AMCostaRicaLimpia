375b0dbf4d3fe565d67ad7aa29450ff5bcf2e39b
Mel Sol <mel.sol26@outlook.com>
2019-06-19 12:48:50 -0600

    Pagina principal, buscar facturas

--- ProyectoAMCRL/ProyectoAMCRL/BusquedaFacturas.aspx.cs
+++ ProyectoAMCRL/ProyectoAMCRL/BusquedaFacturas.aspx.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using BL;
+using System.Drawing;
+using System.Data;
+using System.Windows.Forms;
 
 namespace ProyectoAMCRL
 {
@@ -16,66 +19,122 @@ namespace ProyectoAMCRL
         {
             //try
             //{
-                cargarGrid();
-            Session["idFactura"] = "";
-                //lblError.Visible = false;
-            //}
-            //catch (Exception)
-            //{
-            //    lblError.Text = "<div class=\"alert alert-danger alert - dismissible fade show\" role=\"alert\"> <strong>Error al cargar la información. </strong>Verifique su conexión a internet.<button type = \"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"> <span aria-hidden=\"true\">&times;</span> </button> </div>";
-            //    lblError.Visible = true;
-            //}
+            if (Session["cuentaLogin"] != null)
+            {
+                if (!this.IsPostBack)
+                {
+                    this.buscar();
+                    cargarEncabezados2();
+                    cargarMateriales();
+                    Session["idFactura"] = "";
+                }
+            }
+            else
+            {
+                Response.Redirect("Login.aspx");
+            }
         }
 
-        private void cargarGrid()
+
+        //Se cargan los materiales utilizados en el filtro
+        private void cargarMateriales()
         {
-            //try
-            //{
-                List<BLFactura> listFacturas = new BLManejadorFacturas().facturasVentas();
-               
-                //listaConsultaGV.Columns[1].Visible = false;
-                gridFacturas.DataSource = listFacturas;
+            BLManejadorMateriales manej = new BLManejadorMateriales();
+            DataSet listaMateriales = manej.listarMaterialesBL();
 
-                gridFacturas.DataBind();
-                gridFacturas.HeaderRow.Cells[1].Text = "Código Factura";
-                gridFacturas.HeaderRow.Cells[2].Text = "Bodega";
-                gridFacturas.HeaderRow.Cells[3].Text = "Moneda";
-                gridFacturas.HeaderRow.Cells[4].Text = "Cédula";
-                gridFacturas.HeaderRow.Cells[5].Text = "Monto";
-                gridFacturas.HeaderRow.Cells[6].Text = "Fecha";
-                gridFacturas.HeaderRow.Cells[7].Text = "Tipo Factura";
-                gridFacturas.HeaderRow.Cells[8].Text = "Socio";
 
-                foreach (GridViewRow row in gridFacturas.Rows)
-                {
-                    LinkButton lb = (LinkButton)row.Cells[0].Controls[0];
-                    lb.Text = "Seleccionar";
-                }
+            foreach (DataRow dr in listaMateriales.Tables[0].Rows)
+            {
+                ListItem item = new ListItem();
 
-                gridFacturas.HeaderRow.Cells[2].Visible = false;
-                for (int i = 0; i < gridFacturas.Rows.Count; i++)
-                {
-                    gridFacturas.Rows[i].Cells[2].Visible = false;
-                }
-
-                gridFacturas.HeaderRow.Cells[3].Visible = false;
-                for (int i = 0; i < gridFacturas.Rows.Count; i++)
-                {
-                    gridFacturas.Rows[i].Cells[3].Visible = false;
+                String codigo = Convert.ToString(dr["COD_MATERIAL"]);
+                String nombre = Convert.ToString(dr["NOMBRE_MATERIAL"]);
+                item.Text = nombre;
+                item.Value = codigo;
+                materialDda.Items.Add(item);
             }
+        }
 
-            gridFacturas.HeaderRow.Cells[7].Visible = false;
+        //Busca en DAO y retorna un DataTable
+        private void buscar()
+        {
+            BLManejadorFacturas man = new BLManejadorFacturas();
+            List<BLFactura> list = man.facturasVentas(txtPalabra.Text.Trim());
+
+            gridFacturas.DataSource = list;
+
+            gridFacturas.DataBind();
+            gridFacturas.Visible = true;
+        }
+
+        //Carga los encabezados de la tabla separando el nombre y apellidos
+        private void cargarEncabezados()
+        {
+            gridFacturas.HeaderRow.Cells[0].Text = "Código Factura";
+            gridFacturas.HeaderRow.Cells[1].Text = "Bodega";
+            gridFacturas.HeaderRow.Cells[2].Text = "Moneda";
+            gridFacturas.HeaderRow.Cells[3].Text = "Cédula";
+            gridFacturas.HeaderRow.Cells[4].Text = "Monto";
+            gridFacturas.HeaderRow.Cells[5].Text = "Fecha";
+            gridFacturas.HeaderRow.Cells[6].Text = "Tipo Factura";
+            gridFacturas.HeaderRow.Cells[7].Text = "Nombre";
+            gridFacturas.HeaderRow.Cells[8].Text = "Primer Apellido";
+            gridFacturas.HeaderRow.Cells[9].Text = "Segundo Apellido";
+
+            gridFacturas.HeaderRow.Cells[1].Visible = false;
             for (int i = 0; i < gridFacturas.Rows.Count; i++)
             {
-                gridFacturas.Rows[i].Cells[7].Visible = false;
+                gridFacturas.Rows[i].Cells[1].Visible = false;
             }
-            //}
-            //catch (Exception)
-            //{
-            //    lblError.Text = "<div class=\"alert alert-danger alert - dismissible fade show\" role=\"alert\"> <strong>Error al cargar los datos de la lista. </strong>Por favor recargue la página o vuelva a la página principal.<button type = \"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"> <span aria-hidden=\"true\">&times;</span> </button> </div>";
-            //    lblError.Visible = true;
-            //}
 
+            gridFacturas.HeaderRow.Cells[2].Visible = false;
+            for (int i = 0; i < gridFacturas.Rows.Count; i++)
+            {
+                gridFacturas.Rows[i].Cells[2].Visible = false;
+            }
+
+            gridFacturas.HeaderRow.Cells[6].Visible = false;
+            for (int i = 0; i < gridFacturas.Rows.Count; i++)
+            {
+                gridFacturas.Rows[i].Cells[6].Visible = false;
+            }
+        }
+
+        //Carga los encabezados de la tabla sin separar el nombre y apellidos
+        private void cargarEncabezados2()
+        {
+            gridFacturas.HeaderRow.Cells[0].Text = "Código Factura";
+            gridFacturas.HeaderRow.Cells[1].Text = "Bodega";
+            gridFacturas.HeaderRow.Cells[2].Text = "Moneda";
+            gridFacturas.HeaderRow.Cells[3].Text = "Cédula";
+            gridFacturas.HeaderRow.Cells[4].Text = "Monto";
+            gridFacturas.HeaderRow.Cells[5].Text = "Fecha";
+            gridFacturas.HeaderRow.Cells[6].Text = "Tipo Factura";
+            gridFacturas.HeaderRow.Cells[7].Text = "Socio";
+
+            gridFacturas.HeaderRow.Cells[1].Visible = false;
+            for (int i = 0; i < gridFacturas.Rows.Count; i++)
+            {
+                gridFacturas.Rows[i].Cells[1].Visible = false;
+            }
+
+            gridFacturas.HeaderRow.Cells[2].Visible = false;
+            for (int i = 0; i < gridFacturas.Rows.Count; i++)
+            {
+                gridFacturas.Rows[i].Cells[2].Visible = false;
+            }
+
+            gridFacturas.HeaderRow.Cells[6].Visible = false;
+            for (int i = 0; i < gridFacturas.Rows.Count; i++)
+            {
+                gridFacturas.Rows[i].Cells[6].Visible = false;
+            }
+        }
+
+        protected void gridFact_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gridFacturas.PageIndex = e.NewPageIndex;
+            this.buscar();
         }
 
 
@@ -83,7 +142,23 @@ namespace ProyectoAMCRL
         {
             try
             {
-                string id = gridFacturas.SelectedRow.Cells[1].Text;
+
+                foreach (GridViewRow row in gridFacturas.Rows)
+                {
+                    if (row.RowIndex == gridFacturas.SelectedIndex)
+                    {
+                        row.BackColor = ColorTranslator.FromHtml("#A1DCF2");
+                        row.ToolTip = string.Empty;
+                    }
+                    else
+                    {
+                        row.BackColor = ColorTranslator.FromHtml("#FFFFFF");
+                        row.ToolTip = "Clic para abrir.";
+                    }
+                }
+
+
+                string id = gridFacturas.SelectedRow.Cells[0].Text;
                 Session["idFactura"] = id;
                 Response.Redirect("Factura.aspx");
             }
@@ -94,14 +169,177 @@ namespace ProyectoAMCRL
             }
         }
 
+        protected void gridFact_Sorting(object sender, GridViewSortEventArgs e)
+        {
+
+        }
+
+        protected void gridFact_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                e.Row.Attributes["onclick"] = Page.ClientScript.GetPostBackClientHyperlink(gridFacturas, "Select$" + e.Row.RowIndex);
+                e.Row.ToolTip = "Clic para abrir.";
+            }
+        }
+
+
+        //boton de filtrar
         protected void btnActualizar_Click(object sender, EventArgs e)
         {
-            if((!String.IsNullOrEmpty(montoMinimo.Text) || (!String.IsNullOrWhiteSpace(montoMinimo.Text))) &&
+            BLManejadorFacturas manej = new BLManejadorFacturas();
+
+            //Filtro montos
+            List<BLFactura> listaFiltrada = new List<BLFactura>();
+            if ((!String.IsNullOrEmpty(montoMinimo.Text) || (!String.IsNullOrWhiteSpace(montoMinimo.Text))) &&
                 (!String.IsNullOrEmpty(montoMax.Text) || (!String.IsNullOrWhiteSpace(montoMax.Text))))
+            {
+                listaFiltrada = filtrarMonto(Convert.ToDouble(montoMinimo.Text), Convert.ToDouble(montoMax.Text));
+                cargarGridFiltrada(listaFiltrada);
+                cargarEncabezados2();
+            }
+
+            //Filtro palabras
+            if ((!String.IsNullOrEmpty(txtPalabra.Text) || (!String.IsNullOrWhiteSpace(txtPalabra.Text))))
+            {
+                cargarEncabezados2();
+                cargarGridFiltrada_Table(manej.buscar(txtPalabra.Text.Trim()));
+            }
+
+            //Filtro Fecha
+            if ((!String.IsNullOrEmpty(datepicker.Value) || (!String.IsNullOrWhiteSpace(datepicker.Value))) &&
+                (!String.IsNullOrEmpty(datepicker2.Value) || (!String.IsNullOrWhiteSpace(datepicker2.Value))))
+            {
+                List <BLFactura> rangoFecha = manej.listaRangoFecha(Convert.ToDateTime(datepicker.Value), Convert.ToDateTime(datepicker2.Value));
+
+                if (listaFiltrada.Count == 0)
+                {
+                    foreach (BLFactura blF in rangoFecha)
+                    {
+                        listaFiltrada.Add(blF);
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < rangoFecha.Count; j++)
+                    {
+                        bool a = true;
+
+                        for (int i = 0; i < listaFiltrada.Count; i++)
+                        {
+                            if (listaFiltrada[i].cod_Factura.Equals(rangoFecha[j].cod_Factura))
+                            {
+                                a = false;
+                            }
+                        }
+                        if (a == false)
+                        {
+                            listaFiltrada.Add(rangoFecha[j]);
+                        }
+                    }
+                }
+            }
+
+            //Para filtrar por materiales
+            List<BLFactura> listaMat = manej.facturasVentas("");
+            foreach(BLFactura b in listaMat)
             {
 
             }
 
+            //Filtro tipo
+            if (radioRol.Checked || radioRol2.Checked)
+            {
+                string tipo = "";
+                if(radioRol.Checked)
+                {
+                    tipo = "v";
+                } else
+                {
+                    tipo = "c";
+                }
+                List<BLFactura> listaTipo = manej.facturasTipo(tipo);
+                if (listaFiltrada.Count == 0)
+                {
+                    foreach (BLFactura blF in listaTipo)
+                    {
+                        listaFiltrada.Add(blF);
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < listaTipo.Count; j++)
+                    {
+                        bool a = true;
+
+                        for (int i = 0; i < listaFiltrada.Count; i++)
+                        {
+                            if (listaFiltrada[i].cod_Factura.Equals(listaTipo[j].cod_Factura))
+                            {
+                                a = false;
+                            }
+                        }
+                        if (a == false)
+                        {
+                            listaFiltrada.Add(listaTipo[j]);
+                        }
+                    }
+                }
+            }
+
+
+            //Final
+            if (listaFiltrada.Count == 0)
+            {
+                //cambiar por mensaje de error
+                //cargarGridFiltrada(listaFiltrada);
+            }
+            else
+            {
+                cargarGridFiltrada(listaFiltrada);
+            }
+
+            //para el de nombre completo
+            cargarEncabezados2();
+        }
+
+        private List<BLFactura> filtrarMonto(double monto1, double monto2)
+        {
+            return new BLManejadorFacturas().listaMontos(monto1, monto2);
+        }
+
+        private void cargarGridFiltrada(List<BLFactura> listFacturas)
+        {
+            gridFacturas.DataSource = listFacturas;
+            gridFacturas.DataBind();
+            cargarEncabezados2();
+            gridFacturas.Visible = true;
+        }
+
+        private void cargarGridFiltrada_Table(DataTable list)
+        {
+            gridFacturas.DataSource = list;
+            gridFacturas.DataBind();
+            cargarEncabezados2();
+        }
+
+        protected void materialesDrop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //String idBodega = bodegasDrop.SelectedItem.Value;
+            //cargarMateriales(idBodega);
+        }
+
+        protected void palabraTb_TextChanged(object sender, EventArgs e)
+        {
+            this.buscar();
+        }
+
+        private void txt_Item_Number_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.buscar();
+            }
         }
     }
 }
--- ProyectoAMCRL/ProyectoAMCRL/Principal.aspx
+++ ProyectoAMCRL/ProyectoAMCRL/Principal.aspx
@@ -3,113 +3,98 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
     <link href="ownStyles.css" rel="stylesheet" />
 </asp:Content>
-<asp:Content ID="Content3" ContentPlaceHolderID="breadcrumbBodyHolder" runat="server">
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item active" aria-current="page">Registrar y Modificar Socio</li>
-        </ol>
-    </nav>
-    </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
-    
+<asp:Content ID="Content2" ContentPlaceHolderID="breadcrumbBodyHolder" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="body" runat="server">
+
     <div class="container">
         <%--<form id="form1" runat="server">--%>
-            <br />
-            <br />
-            <br />
-            <div class="row justify-content-center">
-                <div class="col-sm-5 offset-1">
-                    <div class="card" style="width: 18rem;">
-                        <asp:Image ID="imagenSocios" class="card-img-top" runat="server" ImageUrl="~/images/socios.png" />
-                        <div class="card-body">
-                            <h5 class="card-title">Socios de negocio</h5>
-                            <p class="card-text">Agregue o busque los socios de negocio.</p>
-                            <asp:DropDownList class="btn btn-info dropdown-toggle" type="dropdown" ata-toggle="dropdown" aria-haspopup="true" aria-expanded="false" AutoPostBack="True" ID="sociosDrop" runat="server">
-                                <asp:ListItem>Agregar Nuevo</asp:ListItem>
-                                <asp:ListItem>Buscar Socios</asp:ListItem>
-                            </asp:DropDownList>
-                        </div>
-                    </div>
-                </div>
+        <br />
 
-                <div class="col-sm-5 offset-1">
-                    <div class="card" style="width: 18rem;">
-                        <asp:Image ID="imagenCompraVenta" class="card-img-top" runat="server" ImageUrl="~/images/compra.png" />
-                        <div class="card-body">
-                            <h5 class="card-title">Compras o Ventas</h5>
-                            <p class="card-text">Realice compras y ventas</p>
-                            <asp:DropDownList class="btn btn-info dropdown-toggle" type="dropdown" ata-toggle="dropdown" aria-haspopup="true" aria-expanded="false" AutoPostBack="True" ID="compraVentaDrop" runat="server">
-                                <asp:ListItem>Realizar compra</asp:ListItem>
-                                <asp:ListItem>Realizar venta</asp:ListItem>
-                            </asp:DropDownList>
-                        </div>
+
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card bg-light" style="width: 18rem; height: 14rem;">
+                    <div class="card-body">
+                        <h5 class="card-title text-secondary">Socios de negocio</h5>
+                        <p class="card-text">Agregue o busque los socios de negocio.</p>
+                        <a href="#" class="card-link text-primary">Agregar Socio</a>
+                        <br />
+                        <a href="#" class="card-link text-primary">Búsqueda de Socios</a>
                     </div>
                 </div>
             </div>
-            <br />
-            <div class="row">
-                <div class="col-sm-5 offset-1">
-                    <div class="card" style="width: 18rem;">
-                        <asp:Image ID="imagenInventario" class="card-img-top" runat="server" ImageUrl="~/images/inventario.png" />
-                        <div class="card-body">
-                            <h5 class="card-title">Inventario</h5>
-                            <p class="card-text">Maneje bodegas, materiales y realice ajustes de inventario.</p>
-                            <asp:DropDownList class="btn btn-info dropdown-toggle" type="dropdown" ata-toggle="dropdown" aria-haspopup="true" aria-expanded="false" AutoPostBack="True" ID="inventarioDrop" runat="server">
-                                <asp:ListItem>Busqueda de bodegas</asp:ListItem>
-                                <asp:ListItem>Materiales</asp:ListItem>
-                                <asp:ListItem>Ajustes de inventario</asp:ListItem>
-                            </asp:DropDownList>
-                        </div>
-                    </div>
-                </div>
 
-                <div class="col-sm-5 offset-1">
-                    <div class="card" style="width: 18rem;">
-                        <asp:Image ID="imagenFacturas" class="card-img-top" runat="server" ImageUrl="~/images/facturas.png" />
-                        <div class="card-body">
-                            <h5 class="card-title">Facturas</h5>
-                            <p class="card-text">Busque y visualice facturas de compra o venta</p>
-                            <asp:DropDownList class="btn btn-info dropdown-toggle" type="dropdown" ata-toggle="dropdown" aria-haspopup="true" aria-expanded="false" AutoPostBack="True" ID="facturasDrop" runat="server">
-                                <asp:ListItem>Busqueda de facturas</asp:ListItem>
-                            </asp:DropDownList>
-                        </div>
+            <div class="col-lg-4">
+                <div class="card bg-light" style="width: 18rem; height: 14rem;">
+                    <div class="card-body">
+                        <h5 class="card-title text-secondary">Compras o Ventas</h5>
+                        <p class="card-text">Realice compras y ventas</p>
+                        <br />
+                        <a href="#" class="card-link text-primary">Realizar Compra</a>
+                        <br />
+                        <a href="#" class="card-link text-primary">Realice Venta</a>
                     </div>
                 </div>
             </div>
-            <br />
-            <div class="row">
-                <div class="col-sm-5 offset-1">
-                    <div class="card" style="width: 18rem;">
-                        <asp:Image ID="imagenReportes" class="card-img-top" runat="server" ImageUrl="~/images/reportes.png" />
-                        <div class="card-body">
-                            <h5 class="card-title">Reportes</h5>
-                            <p class="card-text">Genere reportes de las entradas y salidas del negocio.</p>
-                            <asp:DropDownList class="btn btn-info dropdown-toggle" type="dropdown" ata-toggle="dropdown" aria-haspopup="true" aria-expanded="false" AutoPostBack="True" ID="reportesDrop" runat="server">
-                                <asp:ListItem>Reporte general</asp:ListItem>
-                                <asp:ListItem>Gráfico</asp:ListItem>
-                            </asp:DropDownList>
-                        </div>
-                    </div>
-                </div>
+            <div class="col-lg-4">
+                <div class="card bg-light" style="width: 18rem; height: 14rem;">
+                    <div class="card-body">
+                        <h5 class="card-title text-secondary">Inventario</h5>
+                        <p class="card-text">Maneje bodegas, materiales y realice ajustes de inventario.</p>
+                        <a href="#" class="card-link text-primary">Búsqueda de bodegas</a>
+                        <br />
+                        <a href="#" class="card-link text-primary">Materiales</a>
+                        <br />
+                        <a href="#" class="card-link text-primary">Ajustes de inventario</a>
+                        <br />
 
-                <div class="col-sm-5 offset-1">
-                    <div class="card" style="width: 18rem;">
-                        <asp:Image ID="imagenOtros" class="card-img-top" runat="server" ImageUrl="~/images/moneda.jpg" />
-                        <div class="card-body">
-                            <h5 class="card-title">Otros</h5>
-                            <p class="card-text">Maneje monedas y unidades de medida del sistema.</p>
-                            <asp:DropDownList class="btn btn-info dropdown-toggle" type="dropdown" ata-toggle="dropdown" aria-haspopup="true" aria-expanded="false" AutoPostBack="True" ID="DropDownList1" runat="server">
-                                <asp:ListItem>Monedas</asp:ListItem>
-                                <asp:ListItem>Unidades de medida</asp:ListItem>
-                            </asp:DropDownList>
-                        </div>
                     </div>
                 </div>
             </div>
-            <br />
-            <br />
+        </div>
 
+        <br />
+        <br />
+        <br />
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card  bg-light" style="width: 18rem; height: 14rem;">
+                <div class="card-body">
+                        <h5 class="card-title text-secondary">Facturas</h5>
+                        <p class="card-text">Busque y visualice facturas de compra o venta</p> 
+                       <a href="#" class="card-link text-primary">Búsqueda de facturas</a>
+                    </div>
+                </div>
+            </div>
 
-        <%--</form>--%>
+            <div class="col-lg-4">
+                <div class="card bg-light" style="width: 18rem; height: 14rem;">
+                    <div class="card-body">
+                        <h5 class="card-title text-secondary">Reportes</h5>
+                        <p class="card-text">Genere reportes de las entradas y salidas del negocio.</p>
+                          <a href="#" class="card-link text-primary">Reporte general</a>
+                        <br />
+                        <a href="#" class="card-link text-primary">Gráfico</a>
+                        <br />
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="card bg-light" style="width: 18rem; height: 14rem;">
+                    <div class="card-body">
+                        <h5 class="card-title text-secondary">Otros</h5>
+                        <p class="card-text">Maneje monedas y unidades de medida del sistema.</p>
+                              <a href="#" class="card-link text-primary">Monedas</a>
+                        <br />
+                        <a href="#" class="card-link text-primary">Unidades de medida</a>
+                        <br />
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
+
 </asp:Content>
